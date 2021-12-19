b7be8f99c129b01ad9ab57df65db3201c1d967c5
khayur <oggurezz@gmail.com>
2021-12-19 19:54:08 +0300

    Initial commit

---
+++ RemoteGallery/RemoteGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RemoteGallery
+//
+//  Created by Yury Khadatovich on 19.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
